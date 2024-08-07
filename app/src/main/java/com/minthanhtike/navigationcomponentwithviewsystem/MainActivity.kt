701fdffdf28f6134447f7556c6b96f4625b26340
minthanhtike <minthanhtike2552@gmail.com>
2024-08-07 12:11:04 +0630

    using navigation dsl with fragment and passing data between fragments

--- app/src/main/java/com/minthanhtike/navigationcomponentwithviewsystem/MainActivity.kt
+++ app/src/main/java/com/minthanhtike/navigationcomponentwithviewsystem/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavType
 import androidx.navigation.createGraph
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.fragment
 
 class MainActivity : AppCompatActivity() {
@@ -20,21 +22,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val navController = findNavController(R.id.nav_host_fragment)
-        navController.graph = navController.createGraph(
-            startDestination = nav_routes.home
-        ) {
-            fragment<HomeFragment>(nav_routes.home) {
-                label = "home_min"
-            }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                as? NavHostFragment
+        navHostFragment?.findNavController()?.let { navController ->
+            navController.graph = navController.createGraph(
+                startDestination = nav_routes.home
+            ) {
+                fragment<HomeFragment>(nav_routes.home) {
+                    label = "home_min"
+                }
 
-            fragment<PlantDetailFragment>("${nav_routes.plant_detail}/{${nav_arguments.plant_id}}") {
-                label = "plant_details"
-                argument(nav_arguments.plant_id) {
-                    type = NavType.StringType
+                fragment<PlantDetailFragment>("${nav_routes.plant_detail}/{${nav_arguments.plant_id}}") {
+                    label = "plant_details"
+                    argument(nav_arguments.plant_id) {
+                        type = NavType.StringType
+                    }
                 }
             }
         }
+
     }
 }
 
--- app/src/main/java/com/minthanhtike/navigationcomponentwithviewsystem/PlantDetailFragment.kt
+++ app/src/main/java/com/minthanhtike/navigationcomponentwithviewsystem/PlantDetailFragment.kt
@@ -5,11 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -24,8 +21,7 @@ class PlantDetailFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            param1 = it.getString(nav_arguments.plant_id)
         }
     }
 
@@ -37,23 +33,9 @@ class PlantDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_plant_detail, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PlantDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PlantDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val text= view.findViewById<TextView>(R.id.data_passing_text)
+        text.text = param1
     }
 }
